7f02444c4cd6a6e1efa8af1fadd0012ead3eff56
Safia Abdalla <safia@safia.rocks>
2016-08-08 16:46:50 -0600

    Add preinstall script

---
+++ build.js
@@ -0,0 +1,15 @@
+const exec = require('child_process').exec;
+
+if (process.platform === 'linux' || process.platform === 'darwin') {
+  exec('./build_libzmq.sh', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    if (err) {
+      throw err;
+    }
+  });
+} else if (process.platform === 'win32') {
+  throw Error('Builds for windows not yet implemented.');
+} else {
+  throw Error('Builds for ' + process.platform + ' are not yet supported.');
+}
