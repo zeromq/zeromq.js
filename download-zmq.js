5131964015d10ee3a4131dfa3e831552e45b15a6
Grant Nestor <grantnestor@gmail.com>
2016-10-12 11:48:36 -0700

    Follow redirect in download-zmq.js

--- download-zmq.js
+++ download-zmq.js
@@ -1,7 +1,22 @@
 var https = require('https');
 var fs = require('fs');
+var url = require('url');
 
-var file = fs.createWriteStream('zeromq-' + process.env.ZMQ + '.tar.gz');
-var request = https.get('https://github.com/' + process.env.ZMQ_REPO + '/releases/download/v' + process.env.ZMQ + '/zeromq-' + process.env.ZMQ + '.tar.gz', function(response) {
-  response.pipe(file);
+var TAR_URL = 'https://github.com/' + process.env.ZMQ_REPO + '/releases/download/v' + process.env.ZMQ + '/zeromq-' + process.env.ZMQ + '.tar.gz';
+var FILE_NAME = 'zeromq-' + process.env.ZMQ + '.tar.gz';
+
+function writeToFile(response) {
+  response.pipe(fs.createWriteStream(FILE_NAME));
+}
+
+https.get(TAR_URL, function(response) {
+  if (response.statusCode > 300 && response.statusCode < 400 && response.headers.location) {
+    if (url.parse(response.headers.location).hostname) {
+      https.get(response.headers.location, writeToFile);
+    } else {
+      https.get(url.resolve(url.parse(TAR_URL).hostname, response.headers.location), writeToFile);
+    }
+  } else {
+    writeToFile(response);
+  }
 });
