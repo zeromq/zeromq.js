598e6877f4d2448d6be57d895958991d667cca15
Lukas Geiger <lukas.geiger94@gmail.com>
2016-11-02 00:28:41 +0100

    Move build_libzmq.sh -> scripts/build_libzmq.sh

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,8 +1,10 @@
+var download = require('./download').download;
+var spawn = require('child_process').spawn;
+var path = require('path');
+var fs = require('fs');
+
 if (process.platform === 'win32') {
   console.log('Downloading libzmq for Windows')
-  var download = require('./download').download;
-  var path = require('path');
-  var fs = require('fs');
 
   var TAR_URL = 'https://github.com/nteract/libzmq-win/releases/download/v1.0.0/libzmq-' + process.arch + '.lib';
   var DIR_NAME = path.join(__dirname, '..', 'windows', 'lib');
@@ -16,12 +18,11 @@ if (process.platform === 'win32') {
 } else {
   console.log('Building libzmq for ' + process.platform)
 
-  var spawn = require('child_process').spawn;
-  var child = spawn('./build_libzmq.sh');
+  var child = spawn('./scripts/build_libzmq.sh');
 
   child.stdout.pipe(process.stdout);
   child.stderr.pipe(process.stderr);
   child.on('error', (err) => {
-  console.log('Failed to start child process.');
-});
+    console.log('Failed to start child process.');
+  });
 }
