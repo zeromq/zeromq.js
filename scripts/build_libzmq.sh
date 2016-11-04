0f13e5a8fe60503514a35dc5a79aef0055553495
Lukas Geiger <lukas.geiger94@gmail.com>
2016-11-04 13:11:55 +0100

    Windows: Bumb libzmq@4.2.0
    And simplify shell script

--- scripts/build_libzmq.sh
+++ scripts/build_libzmq.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 set -e
 
-test -d zmq || mkdir zmq
+if [ "$1" != "" ]; then
+  ZMQ=$1
+else
+  echo "No ZMQ version given"
+  exit 1
+fi
 
-ZMQ=4.2.0
-ZMQ_REPO=zeromq/libzmq
-
-realpath() {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
-export MAIN_DIR="$(dirname $(realpath $0))/../"
-export ZMQ_PREFIX=$MAIN_DIR/zmq
+export ZMQ_PREFIX="$(dirname $0)/../zmq"
 export ZMQ_SRC_DIR=zeromq-$ZMQ
 cd $ZMQ_PREFIX
 
@@ -18,7 +16,6 @@ export CFLAGS=-fPIC
 export CXXFLAGS=-fPIC
 export PKG_CONFIG_PATH=$ZMQ_PREFIX/lib/pkgconfig
 
-test -f zeromq-$ZMQ.tar.gz || ZMQ=$ZMQ ZMQ_REPO=$ZMQ_REPO node $MAIN_DIR/scripts/download-zmq.js
 test -d $ZMQ_SRC_DIR || tar xzf zeromq-$ZMQ.tar.gz
 cd $ZMQ_SRC_DIR
 
--- scripts/download-zmq.js
+++
@@ -1,6 +0,0 @@
-var download = require('./download').download;
-
-var TAR_URL = 'https://github.com/' + process.env.ZMQ_REPO + '/releases/download/v' + process.env.ZMQ + '/zeromq-' + process.env.ZMQ + '.tar.gz';
-var FILE_NAME = 'zeromq-' + process.env.ZMQ + '.tar.gz';
-
-download(TAR_URL, FILE_NAME);
