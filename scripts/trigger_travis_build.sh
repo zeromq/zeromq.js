4915a352809d794cbf95b7fba76d60c5fd48df0c
Lukas Geiger <lukas.geiger94@gmail.com>
2016-12-03 22:03:05 +0100

    Support AppVeyor

--- scripts/trigger_travis_build.sh
+++ scripts/trigger_travis_build.sh
@@ -1,4 +1,4 @@
-body='{
+travis_body='{
 "request": {
   "message": "Test prebuilt binaries",
   "branch": "prebuilt-testing"
@@ -9,5 +9,18 @@ curl -s -X POST \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token $TRAVIS_TOKEN" \
-  -d "$body" \
+  -d "$travis_body" \
   https://api.travis-ci.org/repo/zeromq%2Fzeromq.js/requests
+
+appveyor_body='{
+  "accountName": "zeromq",
+  "projectSlug": "zeromq-js",
+  "branch": "prebuilt-testing",
+}'
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Authorization: Bearer $APPVEYOR_TOKEN" \
+  -d "$appveyor_body" \
+  https://ci.appveyor.com/api/builds
