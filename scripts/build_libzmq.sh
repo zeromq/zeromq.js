0646987183860440026cdb5cad95cde01342994e
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-04-17 11:26:39 +0100

    Build libzmq without documentation
    * This change reduces build times by half.

--- scripts/build_libzmq.sh
+++ scripts/build_libzmq.sh
@@ -23,9 +23,9 @@ cd "${ZMQ_SRC_DIR}"
 
 test -f configure || ./autogen.sh
 if [ "$ZMQ" = "4.1.6" ]; then
-  ./configure "--prefix=${ZMQ_PREFIX}" --with-relaxed --enable-static --disable-shared ;
+  ./configure "--prefix=${ZMQ_PREFIX}" --with-relaxed --enable-static --disable-shared --without-documentation ;
 else
-  ./configure "--prefix=${ZMQ_PREFIX}" --disable-pedantic --enable-static --disable-shared ;
+  ./configure "--prefix=${ZMQ_PREFIX}" --disable-pedantic --enable-static --disable-shared --without-documentation ;
 fi
 make -j 2
 make install
