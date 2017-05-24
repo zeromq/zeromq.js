480ce7c912ced3d6735b6e839640967980e0d032
Kyle Kelley <rgbkrk@gmail.com>
2017-05-24 10:12:21 -0700

    handle the error event from the initial get

--- scripts/download.js
+++ scripts/download.js
@@ -8,28 +8,38 @@ function writeToFile(filename, response, callback) {
 }
 
 function download(fileUrl, filename, callback) {
-  https.get(fileUrl, function(response) {
-    if (
-      response.statusCode > 300 &&
-      response.statusCode < 400 &&
-      response.headers.location
-    ) {
-      if (url.parse(response.headers.location).hostname) {
-        https.get(response.headers.location, function(res) {
-          writeToFile(filename, res, callback);
-        });
-      } else {
-        https.get(
-          url.resolve(url.parse(fileUrl).hostname, response.headers.location),
-          function(res) {
+  https
+    .get(fileUrl, function(response) {
+      if (
+        response.statusCode > 300 &&
+        response.statusCode < 400 &&
+        response.headers.location
+      ) {
+        if (url.parse(response.headers.location).hostname) {
+          https.get(response.headers.location, function(res) {
             writeToFile(filename, res, callback);
-          }
+          });
+        } else {
+          https.get(
+            url.resolve(url.parse(fileUrl).hostname, response.headers.location),
+            function(res) {
+              writeToFile(filename, res, callback);
+            }
+          );
+        }
+      } else {
+        writeToFile(filename, response, callback);
+      }
+    })
+    .on("error", function(err) {
+      console.error(err);
+      if (err.code === "ECONNRESET") {
+        console.error("\n** Your connection was reset. **");
+        console.error(
+          "\n** Are you behind a proxy or a firewall that is preventing a connection? **"
         );
       }
-    } else {
-      writeToFile(filename, response, callback);
-    }
-  });
+    });
 }
 
 module.exports = {
