6b74274addfb3424d65afa6b3fb06ea57389143f
Kyle Kelley <rgbkrk@gmail.com>
2017-05-24 10:06:32 -0700

    run prettier on download.js

--- scripts/download.js
+++ scripts/download.js
@@ -1,23 +1,30 @@
-var https = require('https');
-var fs = require('fs');
-var url = require('url');
+var https = require("https");
+var fs = require("fs");
+var url = require("url");
 
 function writeToFile(filename, response, callback) {
   response.pipe(fs.createWriteStream(filename));
-  response.on('end', callback)
+  response.on("end", callback);
 }
 
 function download(fileUrl, filename, callback) {
   https.get(fileUrl, function(response) {
-    if (response.statusCode > 300 && response.statusCode < 400 && response.headers.location) {
+    if (
+      response.statusCode > 300 &&
+      response.statusCode < 400 &&
+      response.headers.location
+    ) {
       if (url.parse(response.headers.location).hostname) {
         https.get(response.headers.location, function(res) {
           writeToFile(filename, res, callback);
         });
       } else {
-        https.get(url.resolve(url.parse(fileUrl).hostname, response.headers.location), function(res) {
-          writeToFile(filename, res, callback);
-        });
+        https.get(
+          url.resolve(url.parse(fileUrl).hostname, response.headers.location),
+          function(res) {
+            writeToFile(filename, res, callback);
+          }
+        );
       }
     } else {
       writeToFile(filename, response, callback);
