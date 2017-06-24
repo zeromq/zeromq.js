f951a86d730e4f14b5f48099df3c2e4ef81582dc
Sebastian Rager <mail@interpretor.net>
2017-06-24 22:13:41 +0200

    Add full prebuild compatibility
    - Releases are now built also for armv7
    - TODO: Differ between arm versions (needs pull request at prebuild)

--- scripts/cross_compile.sh
+++ scripts/cross_compile.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-ARCH=$1
+TYPE=$1
 GCC=$2
+GH_TOKEN=$3
+
+if [[ "${TYPE}" == "armv6" ]]; then
+  ARCH="arm-linux-gnueabi"
+elif [[ "${TYPE}" == "armv7" ]]; then
+  ARCH="arm-linux-gnueabihf"
+else
+  exit 1
+fi
 
 # Update dependencies
 sudo apt-get -qq update
@@ -9,8 +18,16 @@ sudo apt-get install -y "gcc-${GCC}-${ARCH}" "g++-${GCC}-${ARCH}"
 
 export CC="${ARCH}-gcc-${GCC}"
 export CXX="${ARCH}-g++-${GCC}"
+export STRIP="${ARCH}-strip"
 export ZMQ_BUILD_OPTIONS="--host=${ARCH}"
 
 echo "Build zeromq.js for ${ARCH} with gcc ${GCC}"
 
-npm install "--arch=${ARCH}"
+if [[ -z $3 ]]; then
+  npm install "--arch=${ARCH}"
+else
+  PREBUILD_OPTS="-u ${GH_TOKEN}"
+fi
+
+# Set architecture to arm because prebuild does not differ between arm versions at the moment
+node_modules/prebuild/bin.js "--arch=arm" --all --strip "${PREBUILD_OPTS}"
