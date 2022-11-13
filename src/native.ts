1fa27ae12c3f9e27a31fb73b1adb5b0d91a3b73b
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-11-12 18:11:21 -0800

    feat: build zeromqjs with CMake and vcpkg

--- src/native.ts
+++ src/native.ts
@@ -1,8 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 /* Declare all native C++ classes and methods in this file. */
-const path = require("path")
-module.exports = require("@aminya/node-gyp-build")(path.join(__dirname, ".."))
+import path from "path"
+import fs from "fs"
+const addonParentDir = path.join(
+  __dirname,
+  "..",
+  "build",
+  process.platform,
+  process.arch,
+  "node",
+)
+const addOnAbiDir = fs.readdirSync(addonParentDir)[0]
+const addonPath = path.join(addonParentDir, addOnAbiDir, "addon.node")
+module.exports = require(addonPath)
 
 /**
  * The version of the ØMQ library the bindings were built with. Formatted as
