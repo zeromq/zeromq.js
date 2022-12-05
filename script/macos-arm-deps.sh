4eadc1871b26fb865aa2e5806be99d145533e95c
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-12-04 16:57:08 -0800

    fix: do not install gnutls for macos arm

--- script/macos-arm-deps.sh
+++ script/macos-arm-deps.sh
@@ -5,7 +5,7 @@ set -e
 #! Based on https://github.com/Homebrew/discussions/discussions/2843#discussioncomment-2243610
 
 bottle_tag="arm64_big_sur" # Macos 11 is big sure
-dependencies="libsodium gnutls"
+dependencies="libsodium"
 
 mkdir -p ~/arm-target/bin
 mkdir -p ~/arm-target/brew-cache
@@ -39,13 +39,3 @@ cd "$PREV_PWD"
 # libsodium
 export PATH="$HOME/arm-target/Cellar/libsodium/1.0.18_1/lib:$PATH"
 export PKG_CONFIG_PATH="$HOME/arm-target/Cellar/libsodium/1.0.18_1/lib:$PKG_CONFIG_PATH"
-
-# gnutils
-export GUILE_TLS_CERTIFICATE_DIRECTORY=/Users/runner/arm-target/etc/gnutls/
-export PATH="$HOME/arm-target/opt/gnu-sed/libexec/gnubin:$PATH"
-export PATH="$HOME/arm-target/opt/m4/bin:$PATH"
-export LDFLAGS="-L$HOME/arm-target/opt/readline/lib $LDFLAGS"
-export CPPFLAGS="-I$HOME/arm-target/opt/readline/include $CPPFLAGS"
-export PKG_CONFIG_PATH="$HOME/arm-target/opt/readline/lib/pkgconfig:$PKG_CONFIG_PATH"
-export PKG_CONFIG_PATH="$HOME/arm-target/opt/openssl@1.1/lib/pkgconfig:$PKG_CONFIG_PATH"
-export PKG_CONFIG_PATH="$HOME/arm-target/opt/readline/lib/pkgconfig:$PKG_CONFIG_PATH"
