188cc3a6f01060665f9b84480fe974f5b547352e
Bartel Eerdekens <bartel.eerdekens@constell8.be>
2022-12-05 11:13:23 +0100

    fix: make macos-arm-deps.sh executable + fix paths + fix re-running

--- script/macos-arm-deps.sh
+++ script/macos-arm-deps.sh
@@ -14,12 +14,12 @@ export PATH="$HOME/arm-target/bin:$PATH"
 # Download Homebrew under ~/arm-target
 PREV_PWD="$PWD"
 cd ~/arm-target
-mkdir arm-homebrew
+mkdir -p arm-homebrew
 curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C arm-homebrew
 cd "$PREV_PWD"
 
 # Add arm-brew binary
-ln -s ~/arm-target/arm-homebrew/bin/brew ~/arm-target/bin/arm-brew
+ln -sf ~/arm-target/arm-homebrew/bin/brew ~/arm-target/bin/arm-brew
 
 # Homebrew env variables
 export HOMEBREW_CACHE=~/arm-target/brew-cache
@@ -28,18 +28,19 @@ export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 # Install the given dependencies for the given bottle_tag
 arm-brew fetch --deps --bottle-tag=$bottle_tag $dependencies |
     grep -E "(Downloaded to:|Already downloaded:)" |
+    grep -E ".tar.gz" |
     grep -v pkg-config |
     awk '{ print $3 }' |
-    xargs -n 1 arm-brew install --force-bottle
+    xargs -n 1 arm-brew reinstall --force-bottle
 
 # Install host version of pkg-config so we can call it in the build system
-arm-brew install pkg-config
+arm-brew reinstall pkg-config
 
 # Add the installed binaries/libraries to the path
-export PATH="$HOME/arm-target/arm-homebrew/bin/:$PATH"
-export PATH="$HOME/arm-target/arm-homebrew/lib/:$PATH"
+export PATH="$HOME/arm-target/bin/:$PATH"
+export PATH="$HOME/arm-target/lib/:$PATH"
 
 # libsodium
-SODIUM_PATH=$(~/arm-target/arm-homebrew/bin/pkg-config libsodium --libs)
+SODIUM_PATH=$(~/arm-target/bin/pkg-config libsodium --libs-only-L | sed -e 's/-L//g') # print only -L and replace "-L" itself
 export PATH="$SODIUM_PATH:$PATH"
 export PKG_CONFIG_PATH="$SODIUM_PATH:$PKG_CONFIG_PATH"
