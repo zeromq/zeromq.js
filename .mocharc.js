0429020365a835562eecf872f1330632325c4840
Dan Rose <danoftheroses@gmail.com>
2024-06-13 16:50:23 -0500

    apply prettier changes

--- .mocharc.js
+++ .mocharc.js
@@ -1,8 +1,8 @@
-'use strict'
+"use strict"
 
 module.exports = {
-  require: ['ts-node/register', 'rocha'],
-  spec: ['test/unit/*-test.ts', 'test/unit/compat/*-test.{ts,js}'],
+  require: ["ts-node/register", "rocha"],
+  spec: ["test/unit/*-test.ts", "test/unit/compat/*-test.{ts,js}"],
   "expose-gc": true,
   "experimental-worker": true,
   recursive: true,
