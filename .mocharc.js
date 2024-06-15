c123acf19a6acee682a4b5e1d345e397cf4a6af3
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-06-15 01:36:01 -0700

    test: run the tests in parallel

--- .mocharc.js
+++ .mocharc.js
@@ -2,9 +2,14 @@
 
 module.exports = {
   require: ["ts-node/register", "rocha"],
-  spec: ["test/unit/*-test.ts", "test/unit/compat/*-test.{ts,js}"],
+  spec: [
+    "test/unit/*-test.ts",
+    "test/unit/compat/*-test.js",
+  ],
   "expose-gc": true,
   "v8-expose-gc": true,
   "experimental-worker": true,
   recursive: true,
+  exit: true,
+  parallel: true,
 }
