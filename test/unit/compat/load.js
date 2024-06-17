69f9ebd1db139802481772d4561d16ba3242d2bb
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-06-17 13:28:49 -0700

    fix: update dev-dependencies

--- test/unit/compat/load.js
+++ test/unit/compat/load.js
@@ -1,8 +1,10 @@
 const path = require("path")
 
-module.exports = require(process.env.ZMQ_COMPAT_PATH
-  ? path.resolve(process.cwd(), process.env.ZMQ_COMPAT_PATH)
-  : "../../../src/compat")
+module.exports = require(
+  process.env.ZMQ_COMPAT_PATH
+    ? path.resolve(process.cwd(), process.env.ZMQ_COMPAT_PATH)
+    : "../../../src/compat",
+)
 
 /* Copy capabilities from regular module. */
 module.exports.capability = require("../../../src").capability
