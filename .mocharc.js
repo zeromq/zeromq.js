3ac7a116006b4570b08c49716578f0b55a22c5cd
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-06-17 13:14:35 -0700

    test: remove rocha from Mocha config

--- .mocharc.js
+++ .mocharc.js
@@ -2,15 +2,13 @@
  * @type {import('mocha').MochaOptions}
  */
 const config = {
-  require: ["ts-node/register", "rocha"],
+  require: ["ts-node/register"],
   spec: [
     "test/unit/*-test.ts",
     "test/unit/compat/*-test.js",
   ],
   "expose-gc": true,
   "v8-expose-gc": true,
-  "experimental-worker": true,
-  recursive: true,
   exit: true,
   parallel: true,
 }
