6040e66cc544121ec45de233673547d93192f19f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-06-19 22:59:18 -0700

    chore: update license names + remove license headers

--- src/incoming_msg.cc
+++ src/incoming_msg.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2019 Rolf Timmermans */
+
 #include "./incoming_msg.h"
 
 #include <cassert>
--- src/observer.cc
+++ src/observer.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2019 Rolf Timmermans */
+
 #include "./observer.h"
 
 #include "./context.h"
