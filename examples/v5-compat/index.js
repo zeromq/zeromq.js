7b553eb07a2e7df6bde8a0d306dde3463d7db7c6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2025-01-05 22:53:01 -0800

    docs: update the compat example

--- examples/v5-compat/index.js
+++ examples/v5-compat/index.js
@@ -1,18 +1,36 @@
+/* eslint-disable */
 const zmq = require("zeromq/v5-compat")
 
-const pub = zmq.socket("pub")
-const sub = zmq.socket("sub")
+function main() {
+  const pub = zmq.socket("pub")
+  const sub = zmq.socket("sub")
 
-pub.bind("tcp://*:3456", err => {
-  if (err) {
-    throw err
-  }
-
-  sub.connect("tcp://127.0.0.1:3456")
-
-  pub.send("message")
-
-  sub.on("message", msg => {
-    // Handle received message...
+  pub.on("bind", address => {
+    console.log(`Bound to ${address}`)
   })
-})
+
+  pub.bind("tcp://127.0.0.1:3456", err => {
+    if (err) {
+      throw err
+    }
+
+    sub.connect("tcp://127.0.0.1:3456")
+    console.log("Subscriber connected to tcp://127.0.0.1:3456")
+
+    sub.on("message", msg => {
+      // Handle received message...
+      console.log(`Received message: ${msg.toString()}`)
+    })
+
+    pub.send("message")
+  })
+
+  if (process.env.CI) {
+    // exit after 1 second in CI environment
+    setTimeout(() => {
+      process.exit(0)
+    }, 2000)
+  }
+}
+
+main()
