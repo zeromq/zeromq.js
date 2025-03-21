3c523274688871397468ba982bd89b25a57e4d70
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2025-03-21 15:39:12 -0700

    fix: use node 22 + approve pnpm builds

--- script/install-deps.sh
+++ script/install-deps.sh
@@ -30,7 +30,7 @@ if [ -n "$apt" ]; then
     # install latest nodejs
     mkdir -p /etc/apt/keyrings
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
     apt-get update -qq
     apt-get install -y --no-install-recommends nodejs
 fi
