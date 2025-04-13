8fb35186387a1ece80565419d3ec3fd9bb1e84ee
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2025-04-13 01:00:24 -0700

    feat: add cross-compiling Windows arm via cmake-ts v1

--- script/install.js
+++ script/install.js
@@ -15,21 +15,7 @@ function cmakeTs() {
     )
   }
 
-  // Default args
-  let args = ["nativeonly"]
-
-  if (process.arch !== process.env.npm_config_target_arch || process.env.cross_compiling === "true") {
-    // cross-compilation
-    if (process.platform === "win32") {
-      if (process.env.npm_config_target_arch === "ia32") {
-        args = ["named-configs", "windows-x86"]
-      } else if (process.env.npm_config_target_arch === "arm64") {
-        args = ["named-configs", "windows-arm64"]
-      }
-    }
-  }
-
-  cp.execFileSync(process.execPath, [cmakeTsPath, ...args], {
+  cp.execFileSync(process.execPath, [cmakeTsPath, "build"], {
     stdio: "inherit",
   })
 }
